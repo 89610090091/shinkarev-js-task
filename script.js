da8fa1efb3d8f1a77ba7d66ede549d8b00b0b39b
Aleksandr_k <rzn.russia4912@gmail.com>
2023-07-18 20:12:20 +0300

    // object

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
-// function
-function calc(a, b) {
-    return a + b
+// Создание объектов
+// Литеральное
+const myObj = {};
+// Через оператор new (конструктор)
+const myObj = new Object();
+
+// Добавление свойств / методов
+// через точку
+myObj.myField = 'hello!';
+// через квадратные скобки
+myObj['my-Field'] = 'hello!';
+// при литеральном создании объекта
+const myObj = {
+    myField: 'hello',
+    myMethod: function () {
+        console.log('Hello!');
+    }
+};
+
+// пример с занятия
+const iam = {
+    name: 'Dmitrii',
+    speed: 50,
+    sayHello: function (asd) {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+};
+
+iam.sayHello(asd)
+
+
+// Удаление свойств
+delete myObj.myField;
+delete myObj['myField'];
+
+// Проверка существования свойства у объекта
+if ('myField' in myObj) {
+
+}
+// аналог на будущее
+myObj.hasOwnProperty('myField')
+
+// Перебор всех свойств объекта
+const myObj = {
+    a: 1,
+    b: 2,
+    c: 3,
 }
 
-let result = calc(2, 6)
-
-const result = calc(2, 3)
-
-// arrow function
-const calc = (a, b) => {
-    return a + b;
+for (let field in myObj) {
+    // Какой-то код. Обращаемся к свойству через myObj[field]
 }
-
-// // function extention
-// const fooName =  () => {
-
-// }
-
-// // function declaration
-// function fooName() {
-
-// }
+//или
+Object.keys(myObj).forEach((field) => {
+    // Какой-то код. Обращаемся к свойству через myObj[field]
+})
+// а если нужны сразу значения, то
+Object.values(myObj).forEach((value) => {
+    // Какой-то код. value хранит значения свойств.
+})
 
 
+// В курсе пропущен синтаксис классов, как и понятие "функция-конструктор".
+// Очень важные темы! После курса изучите самостоятельно сначала функции-конструкторы, потом классы.
+// В современном ООП используется именно синтаксис классов.
 
-
-// Замыкание
-function foo1() {
-    const x = 1;
-    return function foo2() {
-        console.log('рузультат', x);
+// синтаксис классов
+class User {
+    name;
+    age;
+    adress;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 }
 
-const foo = foo1();
-foo()
+// функция-конструктор
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
