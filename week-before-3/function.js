f8c86cb4a2db5ad9c6e4eb44d70e1349692bbfcd
Aleksandr_k <rzn.russia4912@gmail.com>
2023-07-28 00:21:27 +0300

    // Closures / function / declaration

---
+++ week-before-3/function.js
@@ -0,0 +1,34 @@
+// function
+function calc(a, b) {
+    return a + b
+}
+
+let result = calc(2, 6)
+
+const result = calc(2, 3)
+
+// arrow function
+const calc = (a, b) => {
+    return a + b;
+}
+
+// function extention
+const fooName = () => {
+
+}
+
+// function declaration
+function fooName() {
+
+}
+
+// Closures 
+function foo1() {
+    const x = 1;
+    return function foo2() {
+        console.log('рузультат', x);
+    }
+}
+
+const foo = foo1();
+foo()
