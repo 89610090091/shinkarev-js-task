eb244e92b53f3cc182dca60ddc1245e1af0b7845
Aleksandr_k <rzn.russia4912@gmail.com>
2023-07-27 18:37:59 +0300

    // function / for / if

---
+++ week-before-3/dz-function.js
@@ -0,0 +1,41 @@
+// 1
+function foo(num1, num2) {
+    if (num1 < num2) {
+        return -1;
+    } else if (num1 > num2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// 2
+function calcFactorial(nam) {
+    if (num === 1 || num === 0) {
+        return 1;
+    }
+
+    for (let index = num - 1; index > 0; index++) {
+        num = num * index;
+    }
+    return num;
+}
+
+// 3 
+function foo(a, b, c) {
+    return Number(`${a}${b}${c}`)
+}
+
+// 4
+function calcArea(a, b) {
+    if (b === undefined) {
+        return a ** 2;
+    }
+
+    return a * b;
+}
+
+// 5
+
+
+
