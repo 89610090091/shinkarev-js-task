194c4b744be2c6b0c93a360be865f6bf22586801
Aleksandr_k <rzn.russia4912@gmail.com>
2023-08-02 09:09:29 +0300

    // function

---
+++ forWorksfile.js
@@ -0,0 +1,13 @@
+const str = 'my  name is Aleksandr';
+
+function foo(str, symbol) {
+    const = indexes = [];
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] === symbol) {
+            indexes.push(index);
+        }
+    }
+
+    console.log('indexes: ', indexes.join(', '));
+    console.log('number of matches: ', indexes.length);
+}
